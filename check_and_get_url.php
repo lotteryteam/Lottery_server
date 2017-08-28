bdb03aa498682a281a20dbb23fa6e769248cf3f9
李冬 <1136855714@qq.com>
2017-08-29 00:46:07 +0800

    update sql injection

--- check_and_get_url.php
+++ check_and_get_url.php
@@ -5,9 +5,24 @@ header("Access-Control-Allow-Origin: *");
 include 'conn.php';
 include 'My.php';
 
+function check_input($value)
+{
+  // 去除斜杠
+  if (get_magic_quotes_gpc())
+    {
+    $value = stripslashes($value);
+    }
+  // 如果不是数字则加引号
+  if (!is_numeric($value))
+    {
+    $value = "'" . mysql_real_escape_string($value) . "'";
+    }
+  return $value;
+}
+
 if (isset($_GET["appid"]) && isset($_GET["type"])) {
-  $appid = $_GET["appid"];
-  $type = $_GET["type"];
+  $appid = check_input($_GET["appid"]);
+  $type = check_input($_GET["type"]);
 
   $sql = "
     SELECT * FROM lottery WHERE appid = '$appid' and type = '$type'

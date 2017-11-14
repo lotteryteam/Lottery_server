d3978b7f4f3ddf401e4b254b7f5de7565ca2490d
李冬 <1136855714@qq.com>
2017-11-14 22:53:14 +0800

    update check_and_get_url.php support memcached

--- check_and_get_url.php
+++ check_and_get_url.php
@@ -2,7 +2,7 @@
 // 允许跨域访问
 header("Access-Control-Allow-Origin: *");
 
-// include 'conn.php';
+include 'conn_memcached.php';
 include 'My.php';
 
 $db_host = 'localhost';
@@ -16,17 +16,54 @@ if (isset($_GET["appid"]) && isset($_GET["type"])) {
   $appid = $_GET["appid"];
   $type = $_GET["type"];
 
+  //缓存服务器中，都是键值对，这里我们设定唯一的键
+  $key = md5($appid);
+
+  $cache_result = array();
+  //根据键，从缓存服务器中获取它的值
+  $cache_result = $mem->get($key);
+  //如果存在该键对应的值，说明缓存中存在该内容
+
   $dbConnection = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8', $db_user, $db_pwd);
 
   $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $stmt = $dbConnection->prepare('SELECT * FROM lottery WHERE appid = :appid and type = :type');
-  $stmt->execute(array(':appid' => $appid, ':type' => $type));
-  foreach ($stmt as $row) {
-    // do something with $row
-    MySuccess($row, 200);
+  if($cache_result){
+    // 已经缓存了
+    // echo "get from memcached";
+    $data_result=$cache_result;
+  } else {
+    // echo "get from mysql";
+    $stmt = $dbConnection->prepare('SELECT * FROM lottery WHERE appid = :appid and type = :type');
+    $stmt->execute(array(':appid' => $appid, ':type' => $type));
+    foreach ($stmt as $row) {
+      $data_result = $row;
+    }
+    $mem->set($key, $data_result, MEMCACHE_COMPRESSED, 3600);
   }
+
+  // update request number
+
+  $sql_request_num = "select request_num from lottery WHERE appid=:appid";
+  $stmt_request_num = $dbConnection->prepare($sql_request_num);
+  $stmt_request_num->execute(array(':appid' => $appid));
+  foreach ($stmt_request_num as $row) {
+    $request_num_result = $row;
+  }
+
+  $request_num = $request_num_result['request_num'];
+
+  $request_num = $request_num + 1;
+
+  $sql = "UPDATE lottery SET request_num=:request_num WHERE appid=:appid";
+  // Prepare statement
+  $stmt2 = $dbConnection->prepare($sql);
+  $stmt2->execute(array(':appid' => $appid, ':request_num' => $request_num));
+  // execute the query
+  $stmt2->execute();
+
+  MySuccess($row, 200);
 
   MyError(101, 201);
 
